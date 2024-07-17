c5eb09d70edaa27224aec4bb3949d6dfdc5cdc1f
caiohobold <caiomarconhobold21@gmail.com>
2024-07-17 18:03:22 -0300

    v1.0.8 - Ajuste nas migrations.

--- Migrations/20240524193402_AjusteTabelas2.cs
+++ Migrations/20240524193402_AjusteTabelas2.cs
@@ -10,48 +10,44 @@ namespace EmprestimosAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Equipamentos_Emprestimos_status",
-                table: "Equipamentos");
+            //migrationBuilder.DropIndex(
+              //  name: "IX_Equipamentos_status",
+               // table: "Equipamentos");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Equipamentos_status",
-                table: "Equipamentos");
-
-            migrationBuilder.DropColumn(
-                name: "status",
-                table: "Equipamentos");
+            //migrationBuilder.DropColumn(
+              //  name: "status",
+               // table: "Equipamentos");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "status",
-                table: "Equipamentos",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
+            //migrationBuilder.AddColumn<int>(
+              //  name: "status",
+                //table: "Equipamentos",
+                //type: "integer",
+               // nullable: false,
+               // defaultValue: 0);
 
-            migrationBuilder.AddColumn<int>(
-                name: "status",
-                table: "Equipamentos",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
+          //  migrationBuilder.AddColumn<int>(
+            //    name: "status",
+              //  table: "Equipamentos",
+                //type: "integer",
+                //nullable: false,
+                //defaultValue: 0);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Equipamentos_status",
-                table: "Equipamentos",
-                column: "status");
+            //migrationBuilder.CreateIndex(
+               // name: "IX_Equipamentos_status",
+              //  table: "Equipamentos",
+              //  column: "status");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_Equipamentos_Emprestimos_status",
-                table: "Equipamentos",
-                column: "status",
-                principalTable: "Emprestimos",
-                principalColumn: "id",
-                onDelete: ReferentialAction.Cascade);
+           // migrationBuilder.AddForeignKey(
+            //    name: "FK_Equipamentos_Emprestimos_status",
+             //   table: "Equipamentos",
+             //   column: "status",
+             //   principalTable: "Emprestimos",
+              //  principalColumn: "id",
+              //  onDelete: ReferentialAction.Cascade);
         }
     }
 }
